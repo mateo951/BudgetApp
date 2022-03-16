a0f2b9724f5e448124a389b3d63428d255d886fe
Mateo Villagómez <mateovillagomez1995@gmail.com>
2022-03-16 12:08:08 -0500

    Add authentication functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
-  resources :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
+  devise_for :users
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+
+  # root 'public_recipes#index'
+  # get 'public_recipes', to: 'public_recipes#index'
+  
+  resources :users
   root "users#index"
 end
