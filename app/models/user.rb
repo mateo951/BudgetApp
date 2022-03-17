fc1f8f860bc0754b9c8c8992ac5e58b8d9e3891f
Mateo Villagómez <mateovillagomez1995@gmail.com>
2022-03-17 00:09:40 -0500

    Add associations/validations for Category

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,9 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
          
-  has_many :categories, foreign_key: 'author_id', dependent: :destroy
-  has_many :activities, foreign_key: 'author_id', dependent: :destroy
-  
+  has_many :categories, dependent: :destroy
+  has_many :activities, dependent: :destroy
+
   validates :name, presence: true
 end
