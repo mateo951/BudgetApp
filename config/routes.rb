21f550ce46f8145a87ff6fb67a12cbb51b721c8b
Mateo Villagómez <mateovillagomez1995@gmail.com>
2022-03-16 21:23:49 -0500

    Add Category model, controllers and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,11 @@
 Rails.application.routes.draw do
+  resources :categories
 
   devise_for :users
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
 
-  # root 'public_recipes#index'
-  # get 'public_recipes', to: 'public_recipes#index'
-  
   resources :users
   root "users#index"
 end
