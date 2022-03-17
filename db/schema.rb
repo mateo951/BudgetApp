02d3e6ee12a0b5adbe724cd9fc6bd6fc2fdf0fb2
Mateo Villagómez <mateovillagomez1995@gmail.com>
2022-03-16 21:37:41 -0500

    Add Activities  model, controllers and views

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_16_163032) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_17_022945) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "activities", force: :cascade do |t|
+    t.string "name"
+    t.decimal "amount", precision: 10, scale: 2
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "author_id"
+    t.index ["author_id"], name: "index_activities_on_author_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.string "icon"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -27,4 +43,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_16_163032) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "activities", "users", column: "author_id"
 end
