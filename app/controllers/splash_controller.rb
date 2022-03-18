09e08c3be467779e716443ca36c889ca2a70d1a3
Mateo Villagómez <mateovillagomez1995@gmail.com>
2022-03-18 12:16:58 -0500

    Add hotfix for devise redirection

--- app/controllers/splash_controller.rb
+++ app/controllers/splash_controller.rb
@@ -1,8 +1,12 @@
 class SplashController < ApplicationController
+  before_action :check_signed_in
   before_action :authenticate_user!, except: [:index]
 
   def index 
-      render :layout => false
-      redirect_to categories_path if user_signed_in?
+    render :layout => false
+  end
+
+  def check_signed_in
+    redirect_to categories_path if signed_in?
   end
 end
