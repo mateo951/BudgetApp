27d69f6db6cb8286060e81b9153fe36579f490d0
Mateo Villagómez <mateovillagomez1995@gmail.com>
2022-03-18 11:16:37 -0500

    Add login view

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,5 +2,11 @@
 import "@hotwired/turbo-rails"
 import "controllers"
 
+let submitBttn = document.querySelector('#login-submit');
+if(submitBttn) {
+  submitBttn.addEventListener('click', function() { document.querySelector('#login-form').submit(); });
+}
+
+
 //= require jquery
 //= require bootstrap
