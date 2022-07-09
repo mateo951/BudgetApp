c3131f4bce2b056fd872545f02fc0aa9003319a5
Mateo Villagomez <mateovillagomez1995@gmail.com>
2022-07-09 11:39:06 -0500

    Add hotfix for categories & Modify css of views

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,22 +1,14 @@
 class CategoriesController < ApplicationController
   load_and_authorize_resource
-  # GET /categories or /categories.json
+
   def index
-    @categories = Category.all
+    @recipes = Category.all.where(user: current_user)
   end
 
-  # GET /categories/1 or /categories/1.json
-  def show; end
-
-  # GET /categories/new
   def new
     @category = Category.new
   end
 
-  # GET /categories/1/edit
-  def edit; end
-
-  # POST /categories or /categories.json
   def create
     @category = Category.new(category_params)
     @category.author_id = current_user.id
@@ -31,20 +23,6 @@ class CategoriesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /categories/1 or /categories/1.json
-  def update
-    respond_to do |format|
-      if @category.update(category_params)
-        format.html { redirect_to category_url(@category), notice: 'Category was successfully updated.' }
-        format.json { render :show, status: :ok, location: @category }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @category.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /categories/1 or /categories/1.json
   def destroy
     @category.destroy
 
@@ -55,13 +33,10 @@ class CategoriesController < ApplicationController
   end
 
   private
-
-  # Use callbacks to share common setup or constraints between actions.
   def set_category
     @category = Category.find(params[:id])
   end
 
-  # Only allow a list of trusted parameters through.
   def category_params
     params.require(:category).permit(:name, :icon)
   end
