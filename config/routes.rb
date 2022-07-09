5ee7080719a3856adfee78894eeb1e71cdf0156a
Mateo Villagomez <mateovillagomez1995@gmail.com>
2022-07-09 10:31:10 -0500

    Add hotfix for forms & devise registration/login

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  devise_scope :user do
-    get '/users/sign_out' => 'devise/sessions#destroy'
-  end
-
+  root "splash#index"
+  
   resources :activities
   resources :categories
-
-  root "splash#index"
 end
